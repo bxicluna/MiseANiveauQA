def81740122104a701b3ecf0f9701ddf334ea957
bxicluna <80334075+bxicluna@users.noreply.github.com>
2025-07-04 12:38:17 +0200

    modif du script

--- math.test.js
+++ math.test.js
@@ -1,9 +1,9 @@
 const{division, addition} = require("./math")
 
 test("diviser 6 par 2", () => {
-    expect(6,2).toBe(3);
+    expect(division(6,2)).toBe(3);
 })
 
 test("additionner 2 + 3", () => {
-    expect(2,3).toBe(5);
+    expect(addition(2,3)).toBe(5);
 })
